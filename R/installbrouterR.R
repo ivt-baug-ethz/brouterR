9b6eed6078f0afb433e7438afe85e8b43b184bb1
mflucas <lucasmdefreitas@hotmail.com>
2022-04-13 15:24:04 +0200

    update
    dfdf

--- R/installbrouterR.R
+++ R/installbrouterR.R
@@ -8,13 +8,9 @@
 #' @examples
 installbrouterR <- function(installationFolderPath=NULL){
 
-
-
   destfile <- paste(tempdir(), "this.zip", sep="/")
   download.file("https://github.com/mflucas/brouterR/raw/master/Resources.zip", destfile=destfile)
   unzip(zipfile = destfile, exdir=installationFolderPath)
-
-  list.files(destfile)
 
   print("Installation complete")
   print("Don't forget to download necessary segments at http://brouter.de/brouter/segments4/ and put them in the segments4 folder before running.")
