011174f65f5ff34a16626c3170ba56b2bdc314bb
mflucas <lucasmdefreitas@hotmail.com>
2022-04-13 15:17:37 +0200

    update Resources
    efe

--- R/installbrouterR.R
+++ R/installbrouterR.R
@@ -8,17 +8,13 @@
 #' @examples
 installbrouterR <- function(installationFolderPath=NULL){
 
-  paths <- .libPaths()
 
-  for(i in 1:length(paths)){
-    path <- paths[i]
-    if(any(list.files(path)=="brouterR")){
 
-      packagePath <- paste(path, "brouterR", sep="/")
-      list.files(packagePath)
+  destfile <- paste(tempdir(), "this.zip", sep="/")
+  download.file("https://github.com/mflucas/brouterR/raw/master/Resources.zip", destfile=destfile)
+  unzip(zipfile = destfile, exdir=installationFolderPath)
 
-    }
-  }
+  list.files(destfile)
 
   print("Installation complete")
   print("Don't forget to download necessary segments at http://brouter.de/brouter/segments4/ and put them in the segments4 folder before running.")
